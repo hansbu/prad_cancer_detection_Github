587e5b5daa9e180c24132fcf056c26750997763b
hansbu <32404085+hansbu@users.noreply.github.com>
2019-06-28 11:33:53 -0400

    initial commit

---
+++ scripts/svs_2_heatmap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ../
+source ./conf/variables.sh
+
+cd data/svs
+nohup bash cp_files.sh &
+cd ../..
+wait;
+
+cd patch_extraction_cancer_40X
+nohup bash start.sh &
+cd ..
+
+cd prediction
+nohup bash start.sh &
+cd ..
+
+wait;
+
+cd heatmap_gen
+nohup bash start.sh &
+cd ..
+
+wait;
+
+exit 0
